8b9e1ce0a1b7933030f7931abf4d0bc922de263f
jonesrasta <jonesrasta@gmail.com>
2026-05-05 16:46:06 -0300

    feat: menu hamburguer ajustes bug II

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,27 @@ export default function App() {
   const current = sections.find((s) => s.id === active)!;
 
   return (
-    <div className="h-screen bg-zinc-100 text-zinc-900 flex flex-col">
+    <div className="h-screen bg-zinc-100 text-zinc-900 flex overflow-x-hidden">
       
-      {/* HEADER */}
       <Header isOpen={isOpen} setOpen={setOpen} />
 
-      {/* CONTEÚDO */}
-      <div className="flex flex-1 overflow-x-hidden">
-        
-        <Sidebar
-          sections={sections}
-          active={active}
-          onChange={setActive}
-          isOpen={isOpen}
-          setOpen={setOpen}
-        />
+      <Sidebar
+        sections={sections}
+        active={active}
+        onChange={setActive}
+        isOpen={isOpen}
+        setOpen={setOpen}
+      />
 
+      {/* layout normal */}
+      <div className="flex flex-1">
         <main className="flex-1 flex items-center justify-center p-2">
           <div className="w-full max-w-6xl">
             <Carousel slides={current.slides} />
           </div>
         </main>
-
       </div>
+
     </div>
   );
 }
